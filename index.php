19b2c9ea391880941c9d1dd132f7c4a64a83ac57
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2020-02-28 12:58:31 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Basic Website</title>
+</head>
+<body>
+
+	<div class="header_section"></div>
+
+	<div class = "navigation_segment"></div>
+
+
+	<div class = "content_section">
+        <h1> Content section</h1>
+
+		<div class = "main_content">
+			<h1>Main Content section</h1>
+
+		</div>
+
+		<div class = "sidebar">
+			<h1>sidebar Content section</h1>
+
+		</div>
+
+			<div class = "footer">
+			<h1>footer Content section</h1>
+
+		</div>
+
+
+
+
+	</div>
+
+</body>
+</html>
