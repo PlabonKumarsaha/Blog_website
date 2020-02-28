16c821330349044eb6b6d19abab1888ad1296299
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2020-02-28 14:07:55 +0600

    Add files via upload

---
+++ dash.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin Dashboard</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<script src="js/jquery-3.4.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<div class="container-fluid">
+	
+	<div class="row">
+
+		<div class="col-sm-2">
+			<h1>PKS</h1>
+
+		</div>
+
+		<div class="col-sm-10">
+
+			<h4>PKS2</h4>
+			<p>thisnkanskanskand<em>janskajj jajadjad</em>> ajdbajdjadn</p>
+			
+		</div>
+			
+		</div>
+		
+
+
+</div>
+	
+
+</body>
+</html>
